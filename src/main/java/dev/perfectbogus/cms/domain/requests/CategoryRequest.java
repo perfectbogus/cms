deb838b58c33cc2ea8d76074140f67359abe98ee
eduardo escobar <war_army_10@hotmail.com>
2021-05-20 21:54:48 -0500

    Add requests package

---
+++ src/main/java/dev/perfectbogus/cms/domain/requests/CategoryRequest.java
@@ -0,0 +1,10 @@
+package dev.perfectbogus.cms.domain.requests;
+
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+
+    String name;
+
+}
--- src/main/java/dev/perfectbogus/cms/domain/resources/CategoryResource.java
+++ src/main/java/dev/perfectbogus/cms/domain/resources/CategoryResource.java
@@ -1,11 +1,10 @@
 package dev.perfectbogus.cms.domain.resources;
 
 import dev.perfectbogus.cms.domain.models.Category;
+import dev.perfectbogus.cms.domain.requests.CategoryRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,5 +21,21 @@ public class CategoryResource {
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
         return ResponseEntity.ok(Arrays.asList(new Category(), new Category()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> newCategory(CategoryRequest category){
+        return new ResponseEntity<>(new Category(), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeCategory(@PathVariable("id") String id){
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") String id, CategoryRequest category){
+        return new ResponseEntity<>(new Category(), HttpStatus.OK);
     }
 }
