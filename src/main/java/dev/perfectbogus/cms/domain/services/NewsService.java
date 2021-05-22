57fc291e384ac8b8c316059611a6d611fdb1b2d4
eduardo escobar <war_army_10@hotmail.com>
2021-05-22 13:20:44 -0500

    Create NewsService and NewsRepository

--- src/main/java/dev/perfectbogus/cms/domain/services/NewsService.java
+++ src/main/java/dev/perfectbogus/cms/domain/services/NewsService.java
@@ -5,6 +5,8 @@ import dev.perfectbogus.cms.domain.repositories.AbstractRepository;
 import dev.perfectbogus.cms.domain.repositories.NewsRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class NewsService extends AbstractRepository<News> {
 
@@ -14,6 +16,24 @@ public class NewsService extends AbstractRepository<News> {
         this.newsRepository = newsRepository;
     }
 
+    public News update(News news){
+        return this.newsRepository.save(news);
+    }
 
+    public News create(News news){
+        return this.newsRepository.save(news);
+    }
 
+    public void delete(String id){
+        final News news = this.newsRepository.findOne(id);
+        this.newsRepository.delete(news);
+    }
+
+    public List<News> findAll(){
+        return this.newsRepository.findAll();
+    }
+
+    public News findOne(String id){
+        return this.newsRepository.findOne(id);
+    }
 }
