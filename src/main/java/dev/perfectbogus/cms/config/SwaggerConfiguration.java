f522114cdf5d9c8e59c4f6c1c24eda9bac34d434
eduardo escobar <war_army_10@hotmail.com>
2021-05-23 16:36:11 -0500

    Adding Swagger Configuration

--- src/main/java/dev/perfectbogus/cms/config/SwaggerConfiguration.java
+++ src/main/java/dev/perfectbogus/cms/config/SwaggerConfiguration.java
@@ -3,7 +3,6 @@ package dev.perfectbogus.cms.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
--- src/main/java/dev/perfectbogus/cms/domain/resources/CategoryResource.java
+++ src/main/java/dev/perfectbogus/cms/domain/resources/CategoryResource.java
@@ -2,6 +2,10 @@ package dev.perfectbogus.cms.domain.resources;
 
 import dev.perfectbogus.cms.domain.models.Category;
 import dev.perfectbogus.cms.domain.requests.CategoryRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,31 +15,57 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
+@Api(tags = "category", description = "Category API")
 public class CategoryResource {
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findOne(@PathVariable("id") String id){
+    @ApiOperation(value = "find category", notes = "Find the category by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Category found"),
+            @ApiResponse(code = 404, message = "Category not found")
+    })
+    public ResponseEntity<Category> findOne(@PathVariable("id") String id) {
         return ResponseEntity.ok(new Category());
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
+    @ApiOperation(value = "List Categories", notes = "List all categories")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Categories found"),
+            @ApiResponse(code = 404, message = "Categories not found")
+    })
+    public ResponseEntity<List<Category>> findAll() {
         return ResponseEntity.ok(Arrays.asList(new Category(), new Category()));
     }
 
     @PostMapping
-    public ResponseEntity<Category> newCategory(CategoryRequest category){
+    @ApiOperation(value = "Create Category", notes = "It permits create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Category Created successfully"),
+            @ApiResponse(code = 400, message = "Invalid Request")
+    })
+    public ResponseEntity<Category> newCategory(CategoryRequest category) {
         return new ResponseEntity<>(new Category(), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeCategory(@PathVariable("id") String id){
-
+    @ApiOperation(value = "Remove Category", notes = "It permits remove a category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Category removed successfully"),
+            @ApiResponse(code = 400, message = "Category not found")
+    })
+    public void removeCategory(@PathVariable("id") String id) {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(@PathVariable("id") String id, CategoryRequest category){
+    @ApiOperation(value = "Update Category", notes = "It permits update category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Category update successfully"),
+            @ApiResponse(code = 404, message = "Category not found"),
+            @ApiResponse(code = 400, message = "invalid request")
+    })
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") String id, CategoryRequest category) {
         return new ResponseEntity<>(new Category(), HttpStatus.OK);
     }
 }
