cb06996bb49d509579f3f47c09df2c6b8b96d746
eduardo escobar <war_army_10@hotmail.com>
2021-05-25 17:07:57 -0500

    Change CategoryRepository to interface that extends from JpaRepository

--- src/main/java/dev/perfectbogus/cms/domain/repositories/CategoryRepository.java
+++ src/main/java/dev/perfectbogus/cms/domain/repositories/CategoryRepository.java
@@ -1,8 +1,15 @@
 package dev.perfectbogus.cms.domain.repositories;
 
 import dev.perfectbogus.cms.domain.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CategoryRepository extends AbstractRepository<Category> {
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+
+    List<Category> findByName(String name);
+
+    List<Category> findByNameIgnoreCaseStartingWith(String name);
+
 }
